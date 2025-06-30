a84c2a14ac591882f65a69de635f175788e4f073
xxhkk3xx <alkabihomoud@gmail.com>
2025-06-30 21:13:06 +0300

    fix: add Content Security Policy headers

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  server: {
+    headers: {
+      'Content-Security-Policy': `
+        default-src 'self';
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://api.lemonsqueezy.com;
+        style-src 'self' 'unsafe-inline';
+        img-src 'self' data: blob: https:;
+        connect-src 'self' https://api.lemonsqueezy.com;
+        frame-src 'self' https://checkout.lemonsqueezy.com;
+      `.replace(/\s+/g, ' ').trim()
+    }
+  },
   build: {
     cssCodeSplit: false,
     assetsInlineLimit: 0,
