be57374ab61311c1d01e512598540e1a4635e837
xxhkk3xx <alkabihomoud@gmail.com>
2025-06-30 22:59:14 +0300

    fix: update vite config for environment variables and base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/QR_Generator/',
   server: {
     headers: {
       'Content-Security-Policy': `
@@ -31,4 +31,9 @@ export default defineConfig({
       },
     },
   },
+  define: {
+    'import.meta.env.VITE_LEMON_SQUEEZY_API_KEY': JSON.stringify(process.env.VITE_LEMON_SQUEEZY_API_KEY),
+    'import.meta.env.VITE_LEMON_SQUEEZY_STORE_ID': JSON.stringify(process.env.VITE_LEMON_SQUEEZY_STORE_ID),
+    'import.meta.env.VITE_LEMON_SQUEEZY_VARIANT_ID': JSON.stringify(process.env.VITE_LEMON_SQUEEZY_VARIANT_ID),
+  }
 })
